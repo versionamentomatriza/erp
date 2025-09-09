06a1f181304c9271b789886107680016302f50ea
Gabriel <versionamentomatriza@gmail.com>
2025-09-09 09:57:05 -0300

    Consertando KernelController

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -82,9 +82,9 @@ class Kernel extends HttpKernel
     ];
 
     protected $routeMiddleware = [
-        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
-        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
-        'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+        'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
 		'verificaAdminParcial' => \App\Http\Middleware\VerificaAdminParcial::class,
     ];
 }
